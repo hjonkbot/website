57c4f7309217f59c95f2472ffb2f9cd421dd7905
Jakub Mańczak <56172798+jakubmanczak@users.noreply.github.com>
2021-04-21 13:20:34 +0200

    :sparkles: :swan: added routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,35 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import Home from '../views/Home.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: '/github',
+    name: 'GitHub',
+    alias: '/gh',
+    redirect: () => {
+      window.location.replace('https://github.com/hjonkbot');
+      return '/github-redirecting';
+    },
+  },
+  {
+    path: '/discord',
+    name: 'Discord',
+    alias: ['dc', 'dsc'],
+    redirect: () => {
+      window.location.replace('https://discord.com'); // TODO: replace with OAuth Discord Bot Invite Link @ejer
+      return '/discord-redirecting';
+    },
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
