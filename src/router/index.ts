39a37efc02c05dfbe99095b26700c2fde77189cb
datejer <ejer.priv@wp.pl>
2021-04-22 14:16:28 +0200

    :swan: Change redirect urls

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,33 +1,35 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import Home from '../views/Home.vue';
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import Home from "../views/Home.vue";
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/github',
-    name: 'GitHub',
-    redirect: () => {
-      window.location.replace('https://github.com/hjonkbot');
-      return '/github-redirecting';
-    },
-  },
-  {
-    path: '/discord',
-    name: 'Discord',
-    redirect: () => {
-      window.location.replace('https://discord.com/api/oauth2/authorize?client_id=705511891715490003&permissions=3411008&scope=bot');
-      return '/discord-redirecting';
-    },
-  },
+	{
+		path: "/",
+		name: "Home",
+		component: Home,
+	},
+	{
+		path: "/github",
+		name: "GitHub",
+		redirect: () => {
+			window.location.replace("https://github.com/hjonkbot");
+			return "/github";
+		},
+	},
+	{
+		path: "/discord",
+		name: "Discord",
+		redirect: () => {
+			window.location.replace(
+				"https://discord.com/api/oauth2/authorize?client_id=705511891715490003&permissions=3411008&scope=bot"
+			);
+			return "/discord";
+		},
+	},
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+	history: createWebHistory(process.env.BASE_URL),
+	routes,
 });
 
 export default router;
