a58920f560ed9c1c5b32ce08deb058f0bf788062
datejer <ejer.priv@wp.pl>
2021-04-22 14:13:39 +0200

    :swan: Switch out swan emoji for goos svg and goos photo <3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,54 +1,59 @@
 <template>
-  <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
-  <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
-  <main>
-    <h1 style="font-size: 8rem">🦢</h1>
-    <h1>Hjönk, World!</h1>
-    <p>
-      hjönkbot is a silly goose with the ability to enter your Voice Chat or
-      slide into your DMs and hjönk away.
-    </p>
-    <div class="btn">
-      <router-link to="/discord">Invite to your Discord Server</router-link>
-    </div>
-  </main>
+	<!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+	<!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+	<main>
+		<img src="../assets/goos.jpg" />
+		<h1>Hjönk, World!</h1>
+		<p>
+			hjönkbot is a silly goose with the ability to enter your Voice Chat or
+			slide into your DMs and hjönk away.
+		</p>
+		<div class="btn">
+			<router-link to="/discord">Invite to your Discord Server</router-link>
+		</div>
+	</main>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue';
-  // import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent } from "vue";
+// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-  export default defineComponent({
-    name: 'Home',
-    // components: {
-    //   HelloWorld,
-    // },
-  });
+export default defineComponent({
+	name: "Home",
+	// components: {
+	//   HelloWorld,
+	// },
+});
 </script>
 
 <style lang="sass" scoped>
+main
+  font-family: 'Architects Daughter', sans-serif
+  padding: 1em 14em
+  h1
+    font-family: 'Gloria Hallelujah', sans-serif
+    text-shadow: 2px 2px 15px rgba(0,0,0,.3)
+  img
+    box-shadow: 2px 2px 15px rgba(0,0,0,.3)
+    height: 8em
+    border-radius: 50%
+    margin-top: 1em
+  .btn
+    a
+      margin: 1.2em
+      display: inline-block
+      background-color: var(--primary)
+      padding: .6em
+      border-radius: 6px
+      color: white
+      text-decoration: inherit
+      box-shadow: 0px 8px 15px rgba(0, 0, 0, .3)
+      transition-duration: .2s
+      &:hover,&:focus
+        background-color: #fff
+        color: #000
+        box-shadow: 0px 0px 7px rgba(0, 0, 0, .3)
+@media only screen and (max-width: 872px)
   main
-    font-family: 'Architects Daughter', sans-serif
-    padding: 1em 14em
-    h1
-      font-family: 'Gloria Hallelujah', sans-serif
-      text-shadow: 2px 2px 15px rgba(0,0,0,.3)
-    .btn
-      a
-        margin: 1.2em
-        display: inline-block
-        background-color: var(--primary)
-        padding: .6em
-        border-radius: 6px
-        color: white
-        text-decoration: inherit
-        box-shadow: 0px 8px 15px rgba(0, 0, 0, .3)
-        transition-duration: .2s
-        &:hover,&:focus
-          background-color: #fff
-          color: #000
-          box-shadow: 0px 0px 7px rgba(0, 0, 0, .3)
-  @media only screen and (max-width: 872px)
-    main
-      padding: 1em 1em
+    padding: 1em 1em
 </style>
